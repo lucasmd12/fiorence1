b16e538815b281c48e3249a39348e88e31fd0014
lucasmd12 <lucasjuzahyder@gmail.com>
2025-08-29 11:11:59 -0300

    Update firebase.js

--- src/static/src/lib/firebase.js
+++ src/static/src/lib/firebase.js
@@ -1,21 +1,21 @@
-import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-// Configuração do Firebaseconst firebaseConfig = {
+// Configuração do Firebase
+const firebaseConfig = {
   apiKey: "AIzaSyDC1J5iV3pN5JaV54eyN1d5aU-MSZms0jA",
   authDomain: "borrachariadeley-76f94.firebaseapp.com",
   projectId: "borrachariadeley-76f94",
-  storageBucket: "borrachariadeley-76f94.firebasestorage.app",
+  storageBucket: "borrachariadeley-76f94.appspot.com", // ✅ corrigido
   messagingSenderId: "228562790598",
   appId: "1:228562790598:web:7bd21ca5daee0b920c8fc7",
   measurementId: "G-R37T8KQCK9"
 };
 
 // Inicializar Firebase
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 
 // Inicializar Firebase Auth
-export const auth = getAuth(app)
+export const auth = getAuth(app);
 
-export default app
-
+export default app;
